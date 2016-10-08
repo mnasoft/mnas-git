ae0a2a224b214d9cdc93199596882826a5c33c96
Nick Matvyeyev <mnasoft@gmail.com>
2016-10-08 21:41:47 +0300

    2016-10-8 21:41:47 mnasoft-00

--- mnas-git-test.lisp
+++ mnas-git-test.lisp
@@ -8,54 +8,9 @@
 
 (git-command "git status" t)
 
+(git-remote-readd t)
+
 (git-commit-a t)
 
-(git-command (concatenate 'string "git push" " "  *m-i* " " "master"))
+(git-command (concatenate 'string "git push" " "  *m-i* " " "master") t )
 
-(make-git-script *clisp-dir* "git pull other master")
-
-(make-git-script *clisp-dir* "git remote remove hp1.zorya.com")
-
-(progn 
-  (make-git-script *clisp-dir* "git add *.lisp *.txt *.asd" )
-  (make-commit-a *clisp-dir*)
-  (make-git-script *clisp-dir* "git push share master")
-  )
-
-(progn
-  (make-git-script *clisp-dir* "git remote remove share")
-  (make-git-clone--bare (concatenate 'string *git-dir* "git/")
-			(concatenate 'string *git-dir* "git-share/") *clisp-dir* "share" "git-clone--bare-share.sh")
-  )
-
-(progn
-  (make-git-script *clisp-dir* "git remote remove other")
-  (make-git-clone--bare (concatenate 'string *git-dir* "git/")
-			(concatenate 'string *git-dir* "git-other/")  *clisp-dir* "other" "git-clone--bare-other.sh")
-  )
-
-(git-remote-add t) ;;;; Добавление удаленных чистых репозиториев
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(directory-namestring  #P"/home/namatv/develop/git/clisp/mnas-git/")
-
-(length (find-filenames-directory *clisp-dir* "\"*.lisp\""))
-
-(length (find-filenames *clisp-dir* "\"*.asd\""))
-
-(find-filenames *clisp-dir* "\".git\"")
-
-(directory-pathname-p (read-line isstr))
-
-(uiop:directorize-pathname-host-device (read-line isstr))
-
-(pathname-directory (uiop:directorize-pathname-host-device (read-line isstr)))
-
-(pathname-type (uiop:directorize-pathname-host-device (read-line isstr)))
-
-(pathname-name (uiop:directorize-pathname-host-device (read-line isstr)))
-
-(pathname-encoding-name (uiop:directorize-pathname-host-device (read-line isstr)))
-
-(file-namestring  (uiop:directorize-pathname-host-device (read-line isstr)))
