1b3be0e5c5f8c2f1b3626d68c89bbfde7b40b1a0
U-ZORYA\namatv <mnasoft@gmail.com>
2017-05-22 16:51:20 +0300

    2017-5-22 16:51:15 KO11-118383

--- mnas-git-vars.lisp
+++ mnas-git-vars.lisp
@@ -10,8 +10,10 @@
       ("mnasoft-00"    "/home/namatv/git--bare/" "/home/namatv/quicklisp/local-projects/"              "")
       ("MNASOFT-01"    "/home/namatv/git--bare/" "E:/PRG/msys64/home/namatv/quicklisp/local-projects/" "")
       ("mnasoft-dev"   "/home/namatv/git--bare/" "/home/namatv/quicklisp/local-projects/"              "")
-      ("hp1.zorya.com" "/_storage/otd11/namatv/develop/" "/_storage/otd11/namatv/develop/git/clisp/"         "/_storage/otd11/namatv/develop/git/clisp/")
-      ("KO11-118383"   "/home/namatv/develop/"           "/home/namatv/develop/git/clisp/"                   "D:/PRG/msys32/home/namatv/develop/git/clisp/"))
+      ("hp1.zorya.com" "/_storage/otd11/namatv/develop/" "/_storage/otd11/namatv/develop/git/clisp/"   "/_storage/otd11/namatv/develop/git/clisp/")
+      ("KO11-118383"   "/home/namatv/git--bare/" "D:/PRG/msys32/home/namatv/quicklisp/local-projects/" "")
+;;;;  ("KO11-118383"   "/home/namatv/develop/"   "/home/namatv/develop/git/clisp/"                     "D:/PRG/msys32/home/namatv/develop/git/clisp/") 
+      )
     "    Список каждым элементом которого является список, 
 относящийся к определенному хосту, состоящий из:
 1 - имени хоста;
