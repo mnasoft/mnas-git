564f58743b049d7426973562b107643826795495
Mykola Matvyeyev <mnasoft@gmail.com>
2023-03-12 20:05:23 +0200

    Sun 12 Mar 2023 06:05:23 PM UTC

--- package.lisp
+++ package.lisp
@@ -4,8 +4,6 @@
   (:documentation "Mnas-Git. Проект предназначен для безсетевого способа синхронизации проектов Common Lisp между различными персональными компьютерами.")
   (:nicknames "MGIT")
   (:use #:cl #:mnas-string #:mnas-path)
-)
+  )
 
-;;;;(declaim (optimize (space 0) (compilation-speed 0)  (speed 0) (safety 3) (debug 3)))
-
-;;;; (declaim (optimize (compilation-speed 0) (debug 3) (safety 0) (space 0) (speed 0)))
+(in-package :mnas-git)
